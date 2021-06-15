0d977213d8d0816e0f7e68a258c9100b9a7dcd3c
Stephen Smith <sasmithjr@gmail.com>
2021-06-14 21:56:05 -0400

    Attempts workaround of reattaching callbacks but no dice

--- plugin/dialog_global_bug/dialogs/local_dialog.rb
+++ plugin/dialog_global_bug/dialogs/local_dialog.rb
@@ -2,9 +2,8 @@ require 'sketchup'
 
 module DialogGlobalBug
   module Dialogs
-    def self.local_dialog
-      dialog = UI::HtmlDialog.new
-
+    def self.add_callbacks(dialog)
+      puts 'in add_callbacks'
       dialog.add_action_callback('myCustomCallback') do |_|
         puts 'my custom callback called'
       end
@@ -18,11 +17,19 @@ module DialogGlobalBug
       dialog.add_action_callback('load_link') do |_, url|
         puts "HtmlDialog#set_url('#{url}')"
         dialog.set_url(url)
+
+        add_callbacks dialog
       end
 
       dialog.add_action_callback('close') do |_|
         dialog.close
       end
+    end
+
+    def self.local_dialog
+      dialog = UI::HtmlDialog.new
+
+      add_callbacks dialog
 
       dialog.set_file(File.join(__dir__, 'html', 'index.html'))
 
